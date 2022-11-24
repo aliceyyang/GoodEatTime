e94663520247a9491fade4d6aa3c07ff80515525
Jessie <orz12588@gmail.com>
2022-11-24 18:46:23 +0800

    restaurant commit

--- src/main/java/restaurant/dao/impl/RestaurantDaoImpl.java
+++ src/main/java/restaurant/dao/impl/RestaurantDaoImpl.java
@@ -3,6 +3,7 @@ package restaurant.dao.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,50 +16,143 @@ import restaurant.vo.RestaurantVO;
 
 public class RestaurantDaoImpl implements RestaurantDao {
 	private DataSource ds;
-	
+
 	public RestaurantDaoImpl() throws NamingException {
 		ds = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/GoodEatTime");
-	}
 
+	}
+	
+	private static final String GET_ALL = "select * from restaurant";
+	private static final String GET_ONE = "select * from restaurant where restaurantNo = ?";
+	private static final String INSERT = "insert into restaurant values(?,?,?,?,?,?,?,?,?,?,?)";
+	private static final String DELETE = "delete from restaurant where restaurantNo = ?";
+	private static final String UPDATE = "update restaurant set restaurantTel=?,restaurantName=?,restaurantTaxIDNo=?,restaurantAccountInfo=?,restaurantBusinessHour=?,restaurantAddr=?,"
+			+ "restaurantStatus=?,restaurantAccount=?,restaurantPassword=?,restaurantCommentQuantity=?,totalCommentRating=? where restaurantNo=?";
+	
+	
 	@Override
+	
 	public void insert(RestaurantVO restaurantVO) {
-		// TODO Auto-generated method stub
+		
+		try(Connection con = ds.getConnection();
+			PreparedStatement ps = con.prepareStatement(INSERT)) {
+			
+			ps.setString(1,restaurantVO.getrestaurantTel());
+			ps.setString(2,restaurantVO.getrestaurantName());
+			ps.setString(3,restaurantVO.getrestaurantTaxIDNo());
+			ps.setString(4,restaurantVO.getrestaurantAccountInfo());
+			ps.setString(5,restaurantVO.getrestaurantBusinessHour());
+			ps.setString(6,restaurantVO.getrestaurantAddr());
+			ps.setBoolean(7,restaurantVO.getrestaurantStatus());
+			ps.setString(8,restaurantVO.getrestaurantAccount());
+			ps.setString(9,restaurantVO.getrestaurantPassword());
+			ps.setInt(10,restaurantVO.getrestaurantCommentQuantity());
+			ps.setInt(11,restaurantVO.getTotalCommentRating());
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Override
 	public void update(RestaurantVO restaurantVO) {
-		// TODO Auto-generated method stub
+		try(Connection con = ds.getConnection();
+			PreparedStatement ps = con.prepareStatement(UPDATE)){
+			
+			ps.setString(1,restaurantVO.getrestaurantTel());
+			ps.setString(2,restaurantVO.getrestaurantName());
+			ps.setString(3,restaurantVO.getrestaurantTaxIDNo());
+			ps.setString(4,restaurantVO.getrestaurantAccountInfo());
+			ps.setString(5,restaurantVO.getrestaurantBusinessHour());
+			ps.setString(6,restaurantVO.getrestaurantAddr());
+			ps.setBoolean(7,restaurantVO.getrestaurantStatus());
+			ps.setString(8,restaurantVO.getrestaurantAccount());
+			ps.setString(9,restaurantVO.getrestaurantPassword());
+			ps.setInt(10,restaurantVO.getrestaurantCommentQuantity());
+			ps.setInt(11,restaurantVO.getTotalCommentRating());
+			ps.setInt(12,restaurantVO.getrestaurantNo());
+			
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Override
 	public void delete(Integer restaurantNo) {
-		// TODO Auto-generated method stub
+		try(Connection con = ds.getConnection();
+			PreparedStatement ps = con.prepareStatement(DELETE)){
+			
+			ps.setInt(1,restaurantNo);
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
+	
 
 	@Override
 	public RestaurantVO findByPrimaryKey(Integer restaurantNo) {
-		// TODO Auto-generated method stub
-		return null;
+		RestaurantVO vo = null;
+		try(Connection con = ds.getConnection();
+			PreparedStatement ps = con.prepareStatement(GET_ONE);
+			ResultSet rs = ps.executeQuery()){
+			
+			ps.setInt(1,restaurantNo);
+			while(rs.next()){
+				vo = new RestaurantVO();
+				
+				vo.setrestaurantNo(rs.getInt(1));
+				vo.setrestaurantTel(rs.getString(2));
+				vo.setrestaurantName(rs.getString(3));
+				vo.setrestaurantTaxIDNo(rs.getString(4));
+				vo.setrestaurantAccountInfo(rs.getString(5));
+				vo.setrestaurantBusinessHour(rs.getString(6));
+				vo.setrestaurantAddr(rs.getString(7));
+				vo.setrestaurantStatus(rs.getBoolean(8));
+				vo.setrestaurantAccount(rs.getString(9));
+				vo.setrestaurantPassword(rs.getString(10));
+				vo.setrestaurantCommentQuantity(rs.getInt(11));
+				vo.setTotalCommentRating(rs.getInt(12));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return vo;
 	}
 
 	@Override
 	public List<RestaurantVO> getAll() {
-		try (
-			Connection conn = ds.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("select * from restaurant");
-			ResultSet rs = pstmt.executeQuery()
-		) {
-			List<RestaurantVO> list = new ArrayList<>();
+		List<RestaurantVO> list = new ArrayList<>();
+		try (Connection con = ds.getConnection();
+				PreparedStatement ps = con.prepareStatement(GET_ALL);
+				ResultSet rs = ps.executeQuery()){
 			while (rs.next()) {
 				RestaurantVO vo = new RestaurantVO();
 				vo.setrestaurantNo(rs.getInt(1));
 				vo.setrestaurantTel(rs.getString(2));
+				vo.setrestaurantName(rs.getString(3));
+				vo.setrestaurantTaxIDNo(rs.getString(4));
+				vo.setrestaurantAccountInfo(rs.getString(5));
+				vo.setrestaurantBusinessHour(rs.getString(6));
+				vo.setrestaurantAddr(rs.getString(7));
+				vo.setrestaurantStatus(rs.getBoolean(8));
+				vo.setrestaurantAccount(rs.getString(9));
+				vo.setrestaurantPassword(rs.getString(10));
+				vo.setrestaurantCommentQuantity(rs.getInt(11));
+				vo.setTotalCommentRating(rs.getInt(12));
+				
 				list.add(vo);
 			}
-			return list;
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return null;
+		return list;
 	}
 }
--- src/main/java/restaurant/service/RestaurantServiceImpl.java
+++ src/main/java/restaurant/service/RestaurantServiceImpl.java
@@ -16,8 +16,69 @@ public class RestaurantServiceImpl implements RestaurantService {
 	}
 	
 	@Override
-	public List<RestaurantVO> findAll() {
+	public List<RestaurantVO> getAll() {
 		return dao.getAll();
 	}
+
+	@Override
+	public RestaurantVO addRestaurant(String restaurantTel, String restaurantName, String restaurantTaxIDNo,
+			String restaurantAccountInfo, String restaurantBusinessHour, String restaurantAddr,
+			Boolean restaurantStatus, String restaurantAccount, String restaurantPassword,
+			Integer restaurantCommentQuantity, Integer totalCommentRating) {
+		
+		RestaurantVO vo = new RestaurantVO();
+		vo.setrestaurantTel(restaurantTel);
+		vo.setrestaurantName(restaurantName);
+		vo.setrestaurantTaxIDNo(restaurantTaxIDNo);
+		vo.setrestaurantAccountInfo(restaurantAccountInfo);
+		vo.setrestaurantBusinessHour(restaurantBusinessHour);
+		vo.setrestaurantAddr(restaurantAddr);
+		vo.setrestaurantStatus(restaurantStatus);
+		vo.setrestaurantAccount(restaurantAccount);
+		vo.setrestaurantPassword(restaurantPassword);
+		vo.setrestaurantCommentQuantity(restaurantCommentQuantity);
+		vo.setTotalCommentRating(totalCommentRating);
+		
+		dao.insert(vo);
+		return vo;
+	}
+
+	@Override
+	public RestaurantVO updateRestaurant(String restaurantTel, String restaurantName,
+			String restaurantTaxIDNo, String restaurantAccountInfo, String restaurantBusinessHour,
+			String restaurantAddr, Boolean restaurantStatus, String restaurantAccount, String restaurantPassword,
+			Integer restaurantCommentQuantity, Integer totalCommentRating,Integer restaurantNO) {
+		
+		RestaurantVO vo = new RestaurantVO();
+		
+		vo.setrestaurantTel(restaurantTel);
+		vo.setrestaurantName(restaurantName);
+		vo.setrestaurantTaxIDNo(restaurantTaxIDNo);
+		vo.setrestaurantAccountInfo(restaurantAccountInfo);
+		vo.setrestaurantBusinessHour(restaurantBusinessHour);
+		vo.setrestaurantAddr(restaurantAddr);
+		vo.setrestaurantStatus(restaurantStatus);
+		vo.setrestaurantAccount(restaurantAccount);
+		vo.setrestaurantPassword(restaurantPassword);
+		vo.setrestaurantCommentQuantity(restaurantCommentQuantity);
+		vo.setTotalCommentRating(totalCommentRating);
+		vo.setrestaurantNo(restaurantNO);
+		
+		dao.update(vo);
+		
+		return vo;
+	}
+
+	@Override
+	public void deleteRestaurant(Integer restaurantNo) {
+		dao.delete(restaurantNo);
+		
+	}
+
+	@Override
+	public RestaurantVO getOneRestaurant(Integer restaurantNo) {
+		return dao.findByPrimaryKey(restaurantNo);
+	}
+
 
 }
