1e25ee2108fdb5b1e51a7d2399ce7b2113fdfd86
Alice <alice.chiehyuyang@gmail.com>
2022-12-08 23:53:53 +0800

    add getter/setter and constructors

--- src/main/java/com/tibame/tga104/member/vo/AdministratorVO.java
+++ src/main/java/com/tibame/tga104/member/vo/AdministratorVO.java
@@ -32,4 +32,81 @@ public class AdministratorVO implements Serializable {
     @Column(name = "verifyAdCoupon")
     private Boolean verifyAdCoupon;
 
+    public AdministratorVO() {
+    }
+
+    public AdministratorVO(Integer adminNo, String adminAccount, String adminPassword, String adminName, Boolean modifyAdminData, Boolean modifyMemberData, Boolean verifyRestaurant, Boolean verifyAdCoupon) {
+        this.adminNo = adminNo;
+        this.adminAccount = adminAccount;
+        this.adminPassword = adminPassword;
+        this.adminName = adminName;
+        this.modifyAdminData = modifyAdminData;
+        this.modifyMemberData = modifyMemberData;
+        this.verifyRestaurant = verifyRestaurant;
+        this.verifyAdCoupon = verifyAdCoupon;
+    }
+
+    public Integer getAdminNo() {
+        return adminNo;
+    }
+
+    public void setAdminNo(Integer adminNo) {
+        this.adminNo = adminNo;
+    }
+
+    public String getAdminAccount() {
+        return adminAccount;
+    }
+
+    public void setAdminAccount(String adminAccount) {
+        this.adminAccount = adminAccount;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public Boolean getModifyAdminData() {
+        return modifyAdminData;
+    }
+
+    public void setModifyAdminData(Boolean modifyAdminData) {
+        this.modifyAdminData = modifyAdminData;
+    }
+
+    public Boolean getModifyMemberData() {
+        return modifyMemberData;
+    }
+
+    public void setModifyMemberData(Boolean modifyMemberData) {
+        this.modifyMemberData = modifyMemberData;
+    }
+
+    public Boolean getVerifyRestaurant() {
+        return verifyRestaurant;
+    }
+
+    public void setVerifyRestaurant(Boolean verifyRestaurant) {
+        this.verifyRestaurant = verifyRestaurant;
+    }
+
+    public Boolean getVerifyAdCoupon() {
+        return verifyAdCoupon;
+    }
+
+    public void setVerifyAdCoupon(Boolean verifyAdCoupon) {
+        this.verifyAdCoupon = verifyAdCoupon;
+    }
 }
