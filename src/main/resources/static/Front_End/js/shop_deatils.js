18e70e20b5ac63b3c014c71e8337d413b11dac1d
Ming <changarm429@gmail.com>
2022-12-12 13:06:10 +0800

    新增商品詳細頁面

---
+++ src/main/resources/static/Front_End/js/shop_deatils.js
@@ -0,0 +1,4 @@
+// console.log("read success");
+
+// 商品星等調整
+$("div.product__details__rating .fill-ratings").attr("style", "width: 70%")
--- src/main/resources/static/Front_End/js/shopping_mall.js
+++ src/main/resources/static/Front_End/js/shopping_mall.js
@@ -1,1 +1,4 @@
-console.log("read success");
+// console.log($("div.product__rating .fill-ratings"));
+
+// 修改商品星等
+$("div.product__rating .fill-ratings").attr("style", "width: 70%")
