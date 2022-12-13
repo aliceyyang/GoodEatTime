811196c786e618a76f147956d1ea2d1c34ac0952
Alice <alice.chiehyuyang@gmail.com>
2022-12-13 14:08:59 +0800

    add CROS to AdOrder & Administrator Controller

--- src/main/java/com/tibame/tga104/member/controller/AdministratorController.java
+++ src/main/java/com/tibame/tga104/member/controller/AdministratorController.java
@@ -1,48 +1,80 @@
 package com.tibame.tga104.member.controller;
 
 import com.tibame.tga104.member.service.AdministratorService;
+import com.tibame.tga104.member.service.MemberService;
 import com.tibame.tga104.member.vo.Administrator;
+import com.tibame.tga104.member.vo.MemberVO;
+import com.tibame.tga104.restaurant.service.RestaurantService;
+import com.tibame.tga104.restaurant.vo.RestaurantVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class AdministratorController {
     @Autowired
     private AdministratorService administratorService;
+    @Autowired
+    private RestaurantService restaurantService;
 
-    @GetMapping("/administrator-read-adminNo")
-    public ResponseEntity<List<Administrator>> getByAll(){
+    @Autowired
+    private MemberService memberService;
+
+
+    @GetMapping("/administrator/user-accounts")
+    public ResponseEntity<List<?>> getUserAccountsByAll() {
+        List<MemberVO> memberList = memberService.getAccountByAll();
+        List<RestaurantVO> restaurantList = restaurantService.getAccountByAll();
+        List list = new ArrayList();
+        Iterator memberIterator = memberList.iterator();
+        while (memberIterator.hasNext()) {
+            list.add(memberIterator.next());
+        }
+        Iterator restaurantIterator = restaurantList.iterator();
+        while (restaurantIterator.hasNext()) {
+            list.add(restaurantIterator.next());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+
+    @GetMapping("/administrator")
+    public ResponseEntity<List<Administrator>> getByAll() {
         List<Administrator> administratorList = administratorService.getByAll();
         return ResponseEntity.status(HttpStatus.OK).body(administratorList);
     }
 
-    @GetMapping("/administrator-read-adminNo/{adminNo}")
-    private ResponseEntity<Administrator> read(@PathVariable Integer adminNo){
+    @GetMapping("/administrator/{adminNo}")
+    private ResponseEntity<Administrator> read(@PathVariable Integer adminNo) {
         Administrator administrator = administratorService.getByAdminNo(adminNo);
         System.out.println("查詢系統管理員編號");
         return ResponseEntity.status(HttpStatus.OK).body(administrator);
     }
-    @GetMapping("/administrator-read-adminAccount/{adminAccount}")
-    private ResponseEntity<Administrator> read(@PathVariable String adminAccount){
+
+    @GetMapping("/administrator/{adminAccount}")
+    private ResponseEntity<Administrator> read(@PathVariable String adminAccount) {
         Administrator administrator = administratorService.getByAdminAccount(adminAccount);
         System.out.println("查詢帳號");
         return ResponseEntity.status(HttpStatus.OK).body(administrator);
     }
 
-    @PostMapping("/administrator-create")
+    @PostMapping("/administrator")
     public ResponseEntity<Administrator> create(@RequestBody Administrator administrator) {
         Integer adminNo = administratorService.insert(administrator);
         Administrator newAccount = administratorService.getByAdminNo(adminNo);
         System.out.println("新增一筆管理員帳號");
         return ResponseEntity.status(HttpStatus.CREATED).body(newAccount);
     }
+
     @PutMapping("/administrator-update/{adminNo}")
     public ResponseEntity<Administrator> update(@PathVariable Integer adminNo,
-                                                @RequestBody Administrator administrator){
+                                                @RequestBody Administrator administrator) {
         administrator.setAdminNo(adminNo);
         administratorService.update(administrator);
         System.out.println("更新帳號資訊");
