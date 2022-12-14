9338d08c4fc679fb4f84bfcd6feb8076cbc37368
Ming <changarm429@gmail.com>
2022-12-14 13:19:13 +0800

    商城頁面新增切換頁面功能、跳轉商品明細頁面

--- src/main/resources/static/Front_End/js/shopping_mall.js
+++ src/main/resources/static/Front_End/js/shopping_mall.js
@@ -6,12 +6,18 @@
 /**
  * 抓全部產品、計算星等後放入
  * 資料格式如下
+ * -> 已完成
  *
- * 12筆一頁分頁???
+ * 12筆一頁分頁
+ * -> 已完成
+ * -> lazy loading???
+ * 
+ * 跳轉至商品明細頁面，呈現商品明細
+ * -> 已完成
  *
  * 依商品類別篩選
  *
- * 加入購物車->跳轉至購物車頁面，修改資料庫中購物車的內容
+ * 跳轉至購物車頁面，修改資料庫中購物車的內容
  *
  * 價格排序
  *
@@ -42,18 +48,24 @@
     }
  ]
  */
+
+//更換頁面的功能
 function changePage(page, data) {
+  // 先刪除原本頁面上的元素
   $("section.shop > div.container > div.row").children().remove();
-  for (var i = 0; i < page * 12; i++) {
+  $("div.shop__pagination").children().remove();
+  // 根據頁面編號放入相對應的產品
+  for (var i = (page - 1) * 12; i < page * 12; i++) {
     let averageRating =
       data[i].prodCommentQty == 0
         ? 0
         : (data[i].totalCommentRating / data[i].prodCommentQty) * 20;
-    let product_html = `<div class="col-lg-3 col-md-6 col-sm-6">
+    let product_html = 
+      `<div class="col-lg-3 col-md-6 col-sm-6">
          <div class="product__item">
            <div
              class="product__item__pic set-bg"
-             style="background-image: url('../product/mainPic?prodNo=${data[i].prodNo}');"
+             style="background-image: url('../product/mainPic?prodNo=${data[i].prodNo}');" data-prodNo="${data[i].prodNo}"
            >
            <div class="product__rating">
              <div class="fill-ratings" style="width: ${averageRating}%">
@@ -68,7 +80,7 @@ function changePage(page, data) {
              </div>
            </div>
            <div class="product__item__text">
-             <h6><a href="#">${data[i].prodName}</a></h6>
+             <h6><a href="#" data-prodNo="${data[i].prodNo}">${data[i].prodName}</a></h6>
              <div class="product__item__price">NTD &nbsp; ${data[i].prodPrice}</div>
              <div class="cart_add" data-prodNo="${data[i].prodNo}">
                <a href="#">加入購物車</a>
@@ -77,11 +89,33 @@ function changePage(page, data) {
          </div>
        </div>`;
     $("section.shop > div.container > div.row").append(product_html);
+    if (data.length == i + 1) {
+      break;
+    }
+  }
+
+  // 更新頁面按鈕
+  if (page > 1) {
+    // 第一頁
+    let first_page = `<a href="#" data-page="1"><span class="arrow_carrot-left"></span></a>`;
+    $("div.shop__pagination").append(first_page);
+  }
+  for (var i = 1; i <= data.length / 12; i++) {
+    // 每一頁
+    let page_html = `<a href="#" data-page="${i}">${i}</a>`;
+    $("div.shop__pagination").append(page_html);
+  }
+  if (data.length - page > 1) {
+    // 最後一頁
+    let last_page = `<a href="#" data-page="last"><span class="arrow_carrot-right"></span></a>`;
+    $("div.shop__pagination").append(last_page);
   }
 }
 
 let prodQty = -1;
 var prodList;
+
+// 載入頁面時先去跟後端拿資料
 $.ajax({
   url: "http://localhost:8080/product/all",
   type: "GET",
@@ -97,8 +131,44 @@ $.ajax({
   },
   complete: function (data) {
     $("div.shop__last__text > p").text(`查詢結果共 ${prodQty} 筆`);
+
     // console.log(prodList)
   },
 });
 
+function goToShopDeatil(prodNo) {
+  window.location.href = `http://localhost:8080/Front_End/shop_details.html?prodNo=${prodNo}`;
+}
 
+$(function () {
+  // 更換頁面事件註冊
+  $("div.shop__pagination").on("click", "a", function (e) {
+    e.preventDefault();
+    // console.log($(this).attr("data-page"));
+    let page = $(this).attr("data-page");
+    if (page != "last") {
+      changePage(page, prodList);
+    } else {
+      // (35/12)|0 = 2; 無條件捨去取整數
+      changePage((prodQty / 12) | 0, prodList);
+    }
+  });
+
+  // 點圖片進入商品明細頁面
+  $("#product_area").on("click", "div.product__item__pic", function (e) {
+    e.preventDefault();
+    // console.log($(this).attr("data-prodNo"));
+    goToShopDeatil($(this).attr("data-prodNo"));
+  });
+
+  // 點商品名稱進入商品明細頁面
+  $("#product_area").on(
+    "click",
+    "div.product__item__text > h6 > a",
+    function (e) {
+      e.preventDefault();
+      goToShopDeatil($(this).attr("data-prodNo"));
+      // console.log($(this).attr("data-prodNo"));
+    }
+  );
+});
