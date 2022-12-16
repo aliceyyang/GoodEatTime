402a750bdf38b73223c51443627152e69b80f36d
Jessie <orz12588@gmail.com>
2022-12-17 00:12:44 +0800

    新增輪播圖片、菜單上傳功能

---
+++ src/main/java/com/tibame/tga104/restaurant/controller/UploadPicController.java
@@ -0,0 +1,53 @@
+package com.tibame.tga104.restaurant.controller;
+
+import java.util.Base64;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.JsonObject;
+import com.tibame.tga104.restaurant.service.MenuService;
+import com.tibame.tga104.restaurant.service.RestaurantCarouselPicService;
+import com.tibame.tga104.restaurant.service.RestaurantPostService;
+import com.tibame.tga104.restaurant.vo.MenuVO;
+import com.tibame.tga104.restaurant.vo.RestaurantCarouselPicVO;
+
+@RestController
+public class UploadPicController {
+	
+	@Autowired
+	private RestaurantCarouselPicService carouselPicService;
+	
+	@PostMapping("/restaurant-uploadMultiplePics/{restaurantNo}")
+	public void uploadMultiplePics(@PathVariable Integer restaurantNo,@RequestBody List<String> carouselPicStr) {
+		for(String str : carouselPicStr) {
+			RestaurantCarouselPicVO vo = new RestaurantCarouselPicVO();
+			
+			byte[] restaurantCarouselPic = Base64.getDecoder().decode(str);
+			
+			vo.setCarouselPic(restaurantCarouselPic);
+			vo.setRestaurantNo(restaurantNo);
+		
+			carouselPicService.addRestaurantCarouselPic(vo);			
+		}
+		
+		
+	}
+	
+	@Autowired
+	private MenuService menuService;
+	
+	@PostMapping("/restaurant-uploadMenu")
+	public void uploadMenu(@RequestBody MenuVO menu) {
+		
+		byte[] MenuPic = Base64.getDecoder().decode(menu.getMenuPicstr());
+		menu.setMenuPic(MenuPic);
+		menuService.addMenu(menu);
+	}
+	
+
+}
