a1fd3fcec4bff04165ee5499bab5bc4aaaf4acf8
Ming <changarm429@gmail.com>
2022-12-22 17:53:07 +0800

    結帳驗證功能更新

--- src/main/resources/static/Front_End/js/checkout.js
+++ src/main/resources/static/Front_End/js/checkout.js
@@ -1,4 +1,4 @@
-console.log("read success");
+// console.log("read success");
 
 /**
  * 抓出購物車傳來的prodOrderDetail資訊呈現在頁面上
@@ -54,76 +54,218 @@ $(function () {
   $("#receiver_name").on("blur", function(){
     $(this).siblings().children(".error_message").remove();
     if ($(this).val().trim()=="") {
+      $(this).removeClass("check_ok");
       let error_message = "<span class='error_message'>&emsp;不可為空白</span>";
       $(this).siblings().append(error_message);
+      return;
     }
+    $(this).addClass("check_ok");
   });
 
   // 收件地址不可為空白
   $("#receiver_address").on("blur", function(){
     $(this).siblings().children(".error_message").remove();
     if ($(this).val().trim()=="") {
+      $(this).removeClass("check_ok");
       let error_message = "<span class='error_message'>&emsp;不可為空白</span>";
       $(this).siblings().append(error_message);
       return;
     }
+    $(this).addClass("check_ok");
   });
 
   // 驗證電話格式
   $("#receiver_tel").on("blur", function(){
     $(this).siblings().children(".error_message").remove();
     if ($(this).val().trim()=="") {
+      $(this).removeClass("check_ok");
       let error_message = "<span class='error_message'>&emsp;不可為空白</span>";
       $(this).siblings().append(error_message);
       return;
     }
     let pattern =  /^09\d{2}\d{6}$/;
     if(!pattern.test($(this).val())){
+      $(this).removeClass("check_ok");
       let error_message = "<span class='error_message'>&emsp;電話號碼格式錯誤</span>";
       $(this).siblings().append(error_message);
       return;
     }
+    $(this).addClass("check_ok");
   });
 
   // 驗證信箱格式
   $("#receiver_mail").on("blur", function(){
     $(this).siblings().children(".error_message").remove();
     if ($(this).val().trim()=="") {
+      $(this).removeClass("check_ok");
       let error_message = "<span class='error_message'>&emsp;不可為空白</span>";
       $(this).siblings().append(error_message);
       return;
     }
     let pattern = /^([\w]+)(.[\w]+)*@([\w]+)(.[\w]{2,3}){1,2}$/;
     if (!pattern.test($(this).val())){
+      $(this).removeClass("check_ok");
       let error_message = "<span class='error_message'>&emsp;信箱地址格式錯誤</span>";
       $(this).siblings().append(error_message);
       return;
     }
+    $(this).addClass("check_ok");
   });
 
   // 驗證統一編號格式
   $("#receiver_taxId").on("blur",function(){
     $(this).siblings().children(".error_message").remove();
     if ($(this).val().trim()=="") {
+      $(this).addClass("check_ok");
       $(this).val("");
       return;
     }
     let pattern = /^\d{8}$/;
     if (!pattern.test($(this).val())){
+      $(this).removeClass("check_ok");
       let error_message = "<span class='error_message'>&emsp;統一編號格式錯誤</span>";
       $(this).siblings().append(error_message);
       return;
     }
+    $(this).addClass("check_ok");
   });
 
+  // StackOverflow的cc_formatter
+  function cc_format(value) {
+    var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
+    var matches = v.match(/\d{4,16}/g);
+    var match = matches && matches[0] || ''
+    var parts = []
+
+    for (i=0, len=match.length; i<len; i+=4) {
+        parts.push(match.substring(i, i+4))
+    }
+
+    if (parts.length) {
+        return parts.join('   ')
+    } else {
+        return value
+    }
+  }
+
+  // 不讓使用者輸入數字以外的東西
+  $("#credit_card").keypress(function (e) {
+    if ((e.which < 48 || e.which > 57) && (e.which !== 8) && (e.which !== 0)) {
+        return false;
+    }
+    return true;
+  });
+
+  $("#credit_card").on("keyup", function(){
+    $(this).val(cc_format($(this).val()));
+    // console.log($(this).val());
+    // console.log($(this).val().replace(/ +/g, ""));
+  });
+
+  // cc_formatter用上面的cc_formatter去改的
+  function expire_date_format(value) {
+    var v = value.replace(/\s+/g, '').replace(/[^0-9]/gi, '')
+    var matches = v.match(/\d{2,4}/g);
+    var match = matches && matches[0] || ''
+    var parts = []
+
+    for (i=0, len=match.length; i<len; i+=2) {
+        parts.push(match.substring(i, i+2))
+    }
+
+    if (parts.length) {
+        return parts.join(' / ')
+    } else {
+        return value
+    }
+  }
+
+  // 不讓使用者輸入數字以外的東西
+  $("#expire_date").keypress(function (e) {
+    if ((e.which < 48 || e.which > 57) && (e.which !== 8) && (e.which !== 0)) {
+        return false;
+    }
+    return true;
+  });
+
+  $("#expire_date").on("keyup", function(){
+    $(this).val(expire_date_format($(this).val()));
+    // console.log($(this).val());
+    // console.log($(this).val().replace(/ +/g, ""));
+  });
+
+  // 不讓使用者輸入數字以外的東西
+  $("#security_code").keypress(function (e) {
+    if ((e.which < 48 || e.which > 57) && (e.which !== 8) && (e.which !== 0)) {
+        return false;
+    }
+    return true;
+    console.log()
+  });
+
+
+  $("#security_code").on("keyup", function(){
+    console.log($(this).val());
+  });
+
+  //確認金額
   $("#price_check").on("click", function(e){
-    if ($("#receiver_name").val().trim() == "") {
-      console.log("aaa");
-      e.stopPropagation();
+    e.stopPropagation();
+    if ($(this).hasClass("price_confirm")) {
+      $(this).find("input").removeAttr("disabled");
+      $(this).find("input").trigger("click");
+      $(this).find("input").attr("disabled", "disabled");
+      $(this).removeClass("price_confirm");
+    } else {
+      $(this).find("input").removeAttr("disabled");
+      $(this).find("input").trigger("click");
+      $(this).find("input").attr("disabled", "disabled");
+      $(this).addClass("price_confirm");
+    }
+    // $(this).toggleClass("price_confirm");
+    // console.log($(this).hasClass("price_confirm"));
+    // console.log("test");
+  });
+
+  // 再次確認收貨人資訊checkbox
+  $("#receiver_check").on("click", function(e){
+    e.stopPropagation();
+    // console.log(!$(".receiver_check").hasClass("check_ok") + "123");
+    if ($(this).hasClass("receiver_confirm")) {
+      $(".receiver_check").removeAttr("disabled");
+      $(this).find("input").removeAttr("disabled");
+      $(this).find("input").trigger("click");
+      $(this).find("input").attr("disabled", "disabled");
+      $(this).removeClass("receiver_confirm");
       return;
     }
-    $(this).toggleClass("-on");
-    // $(this).find("label").trigger("click");
+
+    if ($("#receiver_name").hasClass("check_ok") &&
+    $("#receiver_address").hasClass("check_ok") &&
+    $("#receiver_tel").hasClass("check_ok") &&
+    $("#receiver_mail").hasClass("check_ok") &&
+    $("#receiver_taxId").hasClass("check_ok")) {
+      $(this).find("input").removeAttr("disabled");
+      $(this).find("input").trigger("click");
+      $(this).find("input").attr("disabled", "disabled");
+      $(".receiver_check").attr("disabled", "disabled");
+      $(this).addClass("receiver_confirm")
+      // $("#receiver_name").attr("disabled", "disabled");
+      // $("#receiver_address").attr("disabled", "disabled");
+      // $("#receiver_tel").attr("disabled", "disabled");
+      // $("#receiver_mail").attr("disabled", "disabled");
+      // $("#receiver_taxId").attr("disabled", "disabled");
+      // console.log("aaa");
+      return;
+    }
+    $(".receiver_check").trigger("blur");
+    // console.log("123");
+    // $(this).toggleClass("-on");
+    // console.log($(this).hasClass("-on"));
   });
+
+  
+
+
   console.log(tempOrder);
 });
