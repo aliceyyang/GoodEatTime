513dcdd0396257b8bb02d8c2df4ad2f948a154bb
J <h216222@gmail.com>
2022-12-23 22:54:51 +0800

    登出功能/filter/修正註冊bug

--- src/main/java/com/tibame/tga104/member/dao/impl/RestaurantMemberDAOImpl.java
+++ src/main/java/com/tibame/tga104/member/dao/impl/RestaurantMemberDAOImpl.java
@@ -1,13 +1,16 @@
 package com.tibame.tga104.member.dao.impl;
 
 
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import com.tibame.tga104.member.dao.RestaurantMemberDAO;
+import com.tibame.tga104.member.vo.MemberVO;
 import com.tibame.tga104.member.vo.RestaurantMemberVO;
+import com.tibame.tga104.member.vo.RestaurantRegistVO;
 
 @Repository
 public class RestaurantMemberDAOImpl implements RestaurantMemberDAO {
@@ -29,6 +32,36 @@ public class RestaurantMemberDAOImpl implements RestaurantMemberDAO {
 				.setParameter("restaurantAccount", restaurantAccount)
 				.setParameter("restaurantPassword", restaurantPassword)
 				.uniqueResult();
+	}
+
+
+//===============餐廳會員註冊===============
+	@Override
+	public RestaurantRegistVO insert(RestaurantRegistVO restaurantRegistVO) {
+		// 確認各必填欄位皆輸入
+		if (restaurantRegistVO != null && restaurantRegistVO.getRestaurantNo() == null) {
+			this.getSession().save(restaurantRegistVO);
+			return restaurantRegistVO;
+		}
+		
+		
+		return null;
+	}
+
+
+
+	//查詢是否有此會員帳號存在
+	@Override
+	public RestaurantRegistVO selectByAccount(String restaurantAccount) {
+		//Select * from restaurant where restaurantAccount = ?
+		final String hql = "from RestaurantRegistVO where restaurantAccount = :restaurantAccount ";
+		try {
+			return session.createQuery(hql, RestaurantRegistVO.class)
+					.setParameter("restaurantAccount", restaurantAccount)
+					.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
 	}
 
 }
