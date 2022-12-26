1709cf191c2b68c8816289fae6b56caa8b0c3462
Cindy <cindy5832@gmail.com>
2022-12-26 14:09:33 +0800

    更新controller (修正session part)

--- src/main/java/com/tibame/tga104/reservation/controller/ReservationController.java
+++ src/main/java/com/tibame/tga104/reservation/controller/ReservationController.java
@@ -35,7 +35,7 @@ public class ReservationController {
 	@Autowired
 	private ReservationService reservationService;
 
-	// 餐廳設定訂位人數 這是對的唷
+	// 餐廳設定訂位人數 這是對的唷ok
 //	@PostMapping("restaurant")
 //	public Map<String, Object> restaurant(HttpSession httpSession, @RequestBody List<ReserveTimeVO> reserveTimeList) {
 //		RestaurantMemberVO vo = (RestaurantMemberVO)httpSession.getAttribute("restaurantMemberVO");
@@ -55,10 +55,11 @@ public class ReservationController {
 	}
 //----------------------------------------------------
 
-	// 這是對的唷 餐廳查找訂位設定
+	// 這是對的唷 餐廳查找訂位設定ok
 //	@GetMapping("restaurant/inf")
-//	public List<ReserveTimeVO> table(HttpSession httpSession, @RequestParam Integer weekDay){
-//		return reserveTimeService.findByWeekDay((Integer)httpSession.getAttribute("restaurantNo"), weekDay);
+//	public List<ReserveTimeVO> table(HttpSession httpSession, @RequestParam Integer weekDay) {
+//		RestaurantMemberVO restaurantMemberVO =(RestaurantMemberVO)httpSession.getAttribute("restaurantMemberVO");
+//		return reserveTimeService.findByWeekDay(restaurantMemberVO.getRestaurantNo(), weekDay);
 //	}
 
 	// 這是測試用
@@ -83,11 +84,12 @@ public class ReservationController {
 //		return reservationService.findByMemberNO(member);
 //	}
 // ----------------------------------------------------------------	
-	// 這是對的唷
-//		@GetMapping("restaurant/reserveStatus")
-//		public List<ReservationDetailVO> reserveStatus(HttpSession httpSession, @RequestParam java.sql.Date date){
-//			return reservationService.findByRestaurantNoAndDate((Integer)httpSession.getAttribute("restaurantNo"), date);
-//		}
+	// 這是對的唷ok
+//	@GetMapping("restaurant/reserveStatus")
+//	public List<ReservationDetailVO> reserveStatus(HttpSession httpSession, @RequestParam java.sql.Date date) {
+//		RestaurantMemberVO restaurantMemberVO =(RestaurantMemberVO)httpSession.getAttribute("restaurantMemberVO");
+//		return reservationService.findByRestaurantNoAndDate(restaurantMemberVO.getRestaurantNo(), date);
+//	}
 
 	@GetMapping("restaurant/reserveStatus")
 	public List<ReservationDetailVO> reserveStatus(@RequestParam java.sql.Date date) {
@@ -96,7 +98,7 @@ public class ReservationController {
 
 // --------------------------------
 
-	// 餐廳查找消費者訂位資訊 這是對的唷
+	// 餐廳查找消費者訂位資訊 這是對的唷ok
 //	@GetMapping("restaurant/reserveInf")
 //	public Map<String, Object> reserveInf(HttpSession httpSession, @RequestParam java.sql.Date date) {
 //		RestaurantVO vo = (RestaurantVO)httpSession.getAttribute("restaurantMemberVO");
@@ -112,88 +114,86 @@ public class ReservationController {
 		resultMap.put("data", reservationService.findByReserveDate(1, date));
 		return resultMap;
 	}
-	
-	//----------------------------------
-	// 修改訂位狀態
+
+	// ----------------------------------
+	// 修改訂位狀態ok
+//	@GetMapping("restaurant/statusUpdate")
+//	public boolean reseveStatus(@RequestParam Integer reserveNo, @RequestParam String reserveStatus,
+//			HttpSession session) {
+//		RestaurantMemberVO restaurantMemberVO = (RestaurantMemberVO) session.getAttribute("restaurantMemberVO");
+//		if (restaurantMemberVO.getRestaurantNo() != null) {
+//			return reservationService.changeStatus(reserveNo, reserveStatus);
+//		}
+//		return false;
+//	}
+
 	@GetMapping("restaurant/statusUpdate")
-	public boolean reseveStatus(@RequestParam Integer reserveNo , @RequestParam String reserveStatus) {
+	public boolean reseveStatus(@RequestParam Integer reserveNo, @RequestParam String reserveStatus) {
 		return reservationService.changeStatus(reserveNo, reserveStatus);
 	}
-	
-	// 消費者訂位
+
+	// 消費者訂位ok
 //	@PostMapping("member")
 //	public boolean member(@RequestBody ReservationVO vo, HttpSession httpSession) {
 //		MemberVO memberVO = (MemberVO)httpSession.getAttribute("memberVO");
 //		Integer member = memberVO.getMemberNo();
 //		if(vo != null && member != null) {
-//			return reservationService.reservation(vo);	
+//			if (vo.getReserveNum() <= reserveTimeService.getAvailableSeats(vo.getRestaurantNo(), vo.getReserveDate(),
+//					vo.getReserveTime())) {
+//				return reservationService.reservation(vo);
+//			}	
 //		}
 //		return false;
 //	}
-	
+
 	@PostMapping("member")
 	public boolean member(@RequestBody ReservationVO vo) {
-		if(vo != null && vo.getMemberNo() != null) {
-			if (vo.getReserveNum() <= reserveTimeService.getAvailableSeats(vo.getRestaurantNo(), vo.getReserveDate(), vo.getReserveTime())) {
+		if (vo != null && vo.getMemberNo() != null) {
+			if (vo.getReserveNum() <= reserveTimeService.getAvailableSeats(vo.getRestaurantNo(), vo.getReserveDate(),
+					vo.getReserveTime())) {
 				return reservationService.reservation(vo);
-			}	
+			}
 		}
 		return false;
 	}
 
-	//--------------------------
+	// --------------------------查找營業時間 (星期)ok
 //	@GetMapping("restaurant/date")
-//	public List<ReserveTimeVO> reserveTime(HttpSession session) {
-//		Integer restaurantNo = (Integer) session.getAttribute("restaurantNo");
+//	public List<Integer> reserveTime(HttpSession session) {
+//		RestaurantMemberVO restaurantMemberVO = (RestaurantMemberVO) session.getAttribute("restaurantMemberVO");
+//		int restaurantNo = restaurantMemberVO.getRestaurantNo();
 //		return reserveTimeService.findByRestaurantNo(restaurantNo);
 //	}
-	
+
 	// wrong
 	@GetMapping("restaurant/date")
 	public List<Integer> reserveTime() {
 		return reserveTimeService.findByRestaurantNo(2);
 	}
-	
-	//-----------查找可訂位人數
-//	@GetMapping("restaurant/seat")
-//	public int seats(@RequestParam java.sql.Date reserveDate, @RequestParam String reserveTime, HttpSession session) {
-//		Integer restaurantNo = (Integer)session.getAttribute("restaurantNo");
-//		return reserveTimeService.getAvailableSeats(restaurantNo, reserveDate, reserveTime);
+
+	// -----------查找可訂位人數ok
+//	@PostMapping("restaurant/seat")
+//	public int seats(@RequestBody ReservationVO vo, HttpSession session) {
+//		RestaurantMemberVO restaurantMemberVO = (RestaurantMemberVO) session.getAttribute("restaurantMemberVO");
+//		int restaurantNo = restaurantMemberVO.getRestaurantNo();
+//		return reserveTimeService.getAvailableSeats(restaurantNo, vo.getReserveDate(), vo.getReserveTime());
 //	}
-	
-	@PostMapping("restaurant/seat")
-	public Integer seats(@RequestBody ReservationVO vo) {
-		return reserveTimeService.getAvailableSeats(2, vo.getReserveDate(), vo.getReserveTime());
-	}
-	
-//-----------------更新訂位成功但未報到的資料
-	
-//	@PostMapping("restaurant/statusChange")
-//	public boolean statusChange(@RequestBody ReservationVO vo) {
-//		if(vo.getReserveDate() != null) {
-//			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//			try {
-//				java.util.Date today = sdf.parse(sdf.format(new java.util.Date()));
-//				java.util.Date date = sdf.parse(sdf.format(vo.getReserveDate()));
-//				if(date.before(today)){
-//					return reservationService.changeStatus(1, vo.getReserveDate());
-//				}
-//			} catch (ParseException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		return false;
+
+//	@PostMapping("restaurant/seat")
+//	public Integer seats(@RequestBody ReservationVO vo) {
+//		return reserveTimeService.getAvailableSeats(2, vo.getReserveDate(), vo.getReserveTime());
 //	}
-	
+
+//-----------------更新訂位成功但未報到的資料ok
+
 	@PostMapping("restaurant/statusChange")
-	public boolean statusChange(@RequestBody ReservationVO vo, HttpSession session) {
-		RestaurantMemberVO restaurantMemberVO = (RestaurantMemberVO)session.getAttribute("restaurantMemberVO");
-		if(vo.getReserveDate() != null) {
+	public boolean statusChange(@RequestBody ReservationVO vo) {
+		if (vo.getReserveDate() != null) {
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			try {
 				java.util.Date today = sdf.parse(sdf.format(new java.util.Date()));
 				java.util.Date date = sdf.parse(sdf.format(vo.getReserveDate()));
-				if(date.before(today)){
+				if (date.before(today)) {
 					return reservationService.changeStatus(1, vo.getReserveDate());
 				}
 			} catch (ParseException e) {
@@ -202,6 +202,24 @@ public class ReservationController {
 		}
 		return false;
 	}
-	
-	
+
+//	@PostMapping("restaurant/statusChange")
+//	public boolean statusChange(@RequestBody ReservationVO vo, HttpSession session) {
+//		RestaurantMemberVO restaurantMemberVO = (RestaurantMemberVO)session.getAttribute("restaurantMemberVO");
+//		int restaurantNo = restaurantMemberVO.getRestaurantNo();
+//		if(vo.getReserveDate() != null) {
+//			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//			try {
+//				java.util.Date today = sdf.parse(sdf.format(new java.util.Date()));
+//				java.util.Date date = sdf.parse(sdf.format(vo.getReserveDate()));
+//				if(date.before(today)){
+//					return reservationService.changeStatus(restaurantNo, vo.getReserveDate());
+//				}
+//			} catch (ParseException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		return false;
+//	}
+
 }
