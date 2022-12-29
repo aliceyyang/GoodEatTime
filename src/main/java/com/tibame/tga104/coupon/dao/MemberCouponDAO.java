90cfcc7843c3ec67b8c19311ecc24a59bcf64757
WilliamYu <willy861109@gmail.com>
2022-12-29 19:22:01 +0800

    新增 會員新增優惠券功能

--- src/main/java/com/tibame/tga104/coupon/dao/MemberCouponDAO.java
+++ src/main/java/com/tibame/tga104/coupon/dao/MemberCouponDAO.java
@@ -3,16 +3,18 @@ package com.tibame.tga104.coupon.dao;
 import java.util.List;
 
 import com.tibame.tga104.coupon.vo.MemberCouponVO;
+import com.tibame.tga104.member.vo.MemberVO;
 
 public interface MemberCouponDAO {
 
 	public List<MemberCouponVO> selectAllCouponByMemberNo(Integer memberNo);
 
-	public MemberCouponVO insert(Integer couponNo);
+	public MemberCouponVO insert(Integer memberNo, Integer couponNo);
 	
 	public boolean update(MemberCouponVO memberCouponVO);
 	
 	public boolean delete(Integer memberNo);
 	
 	public List<MemberCouponVO> getAll();
+
 }
