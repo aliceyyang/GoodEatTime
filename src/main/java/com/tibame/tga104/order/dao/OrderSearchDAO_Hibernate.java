da679f01104cee0329c2ee1e5578db10c7aa3402
Ming <changarm429@gmail.com>
2022-12-31 17:22:43 +0800

    訂單呈現相關後端程式更新

--- src/main/java/com/tibame/tga104/order/dao/OrderSearchDAO_Hibernate.java
+++ src/main/java/com/tibame/tga104/order/dao/OrderSearchDAO_Hibernate.java
@@ -31,6 +31,15 @@ public class OrderSearchDAO_Hibernate implements OrderSearchDAO_interface {
 	}
 	
 	@Override
+	public List<OrderSearchVO> selectByProdOrderNo(Integer prodOrderNo) {
+		if(prodOrderNo == null || prodOrderNo < 1) {
+			return null;
+		}
+		String hql = "from OrderSearchVO where prodOrderNo = :prodOrderNo";
+		return this.getSession().createQuery(hql, OrderSearchVO.class).setParameter("prodOrderNo", prodOrderNo).list();
+	}
+	
+	@Override
 	public List<OrderSearchVO> selectByConditions(OrderSearchVO orderSearchVO) {
 		return getSession().createCriteria(OrderSearchVO.class)
 			.add(Example.create(orderSearchVO))
@@ -42,4 +51,25 @@ public class OrderSearchDAO_Hibernate implements OrderSearchDAO_interface {
 		
 		return this.getSession().createQuery("from OrderSearchVO", OrderSearchVO.class).list();
 	}
+
+	@Override
+	public List<OrderSearchVO> selectByMemberNo(Integer memberNo) {
+		if (memberNo == null || memberNo < 1) {
+			return null;
+		}
+		String hql = "from OrderSearchVO where memberNo = :memberNo";
+		return this.getSession().createQuery(hql, OrderSearchVO.class).setParameter("memberNo", memberNo).list();
+		
+	}
+
+	@Override
+	public List<OrderSearchVO> selectByRestaurantNo(Integer restaurantNo) {
+		if (restaurantNo == null || restaurantNo < 1) {
+			return null;
+		}
+		String hql = "from OrderSearchVO where restaurantNo = :restaurantNo";
+		return this.getSession().createQuery(hql, OrderSearchVO.class).setParameter("restaurantNo", restaurantNo).list();
+	}
+
+	
 }
--- src/main/java/com/tibame/tga104/order/vo/OrderSearchVO.java
+++ src/main/java/com/tibame/tga104/order/vo/OrderSearchVO.java
@@ -50,6 +50,11 @@ public class OrderSearchVO implements java.io.Serializable{
 	private String invoiceNumber;
 	@Column(name="taxIDNumber")
 	private String taxIDNumber;
+	@Column(name="restaurantName")
+	private String restaurantName;
+	@Column(name="couponName")
+	private String couponName;
+	@Id
 	@Column(name="prodNo")
 	private Integer prodNo;
 	@Column(name="prodName")
@@ -62,8 +67,162 @@ public class OrderSearchVO implements java.io.Serializable{
 	private String name;
 	@Column(name="tel")
 	private String tel;
-	
-	
+	public Integer getProdOrderNo() {
+		return prodOrderNo;
+	}
+	public void setProdOrderNo(Integer prodOrderNo) {
+		this.prodOrderNo = prodOrderNo;
+	}
+	public Integer getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(Integer memberNo) {
+		this.memberNo = memberNo;
+	}
+	public Integer getRestaurantNo() {
+		return restaurantNo;
+	}
+	public void setRestaurantNo(Integer restaurantNo) {
+		this.restaurantNo = restaurantNo;
+	}
+	public Integer getCouponNo() {
+		return couponNo;
+	}
+	public void setCouponNo(Integer couponNo) {
+		this.couponNo = couponNo;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public java.sql.Timestamp getProdOrderDate() {
+		return prodOrderDate;
+	}
+	public void setProdOrderDate(java.sql.Timestamp prodOrderDate) {
+		this.prodOrderDate = prodOrderDate;
+	}
+	public java.sql.Timestamp getProdOrderReveiveTime() {
+		return prodOrderReveiveTime;
+	}
+	public void setProdOrderReveiveTime(java.sql.Timestamp prodOrderReveiveTime) {
+		this.prodOrderReveiveTime = prodOrderReveiveTime;
+	}
+	public java.sql.Timestamp getProdOderDeliverTime() {
+		return prodOderDeliverTime;
+	}
+	public void setProdOderDeliverTime(java.sql.Timestamp prodOderDeliverTime) {
+		this.prodOderDeliverTime = prodOderDeliverTime;
+	}
+	public Integer getDeliverFee() {
+		return deliverFee;
+	}
+	public void setDeliverFee(Integer deliverFee) {
+		this.deliverFee = deliverFee;
+	}
+	public Integer getAmountBeforeCoupon() {
+		return amountBeforeCoupon;
+	}
+	public void setAmountBeforeCoupon(Integer amountBeforeCoupon) {
+		this.amountBeforeCoupon = amountBeforeCoupon;
+	}
+	public Integer getAmountAfterCoupon() {
+		return amountAfterCoupon;
+	}
+	public void setAmountAfterCoupon(Integer amountAfterCoupon) {
+		this.amountAfterCoupon = amountAfterCoupon;
+	}
+	public Integer getProdOrderPoint() {
+		return prodOrderPoint;
+	}
+	public void setProdOrderPoint(Integer prodOrderPoint) {
+		this.prodOrderPoint = prodOrderPoint;
+	}
+	public String getProdOrderReceiverName() {
+		return prodOrderReceiverName;
+	}
+	public void setProdOrderReceiverName(String prodOrderReceiverName) {
+		this.prodOrderReceiverName = prodOrderReceiverName;
+	}
+	public String getProdOrderReceiverTel() {
+		return prodOrderReceiverTel;
+	}
+	public void setProdOrderReceiverTel(String prodOrderReceiverTel) {
+		this.prodOrderReceiverTel = prodOrderReceiverTel;
+	}
+	public String getProdOrderReceiverMail() {
+		return prodOrderReceiverMail;
+	}
+	public void setProdOrderReceiverMail(String prodOrderReceiverMail) {
+		this.prodOrderReceiverMail = prodOrderReceiverMail;
+	}
+	public String getProdOrderReceiverAddress() {
+		return prodOrderReceiverAddress;
+	}
+	public void setProdOrderReceiverAddress(String prodOrderReceiverAddress) {
+		this.prodOrderReceiverAddress = prodOrderReceiverAddress;
+	}
+	public String getInvoiceNumber() {
+		return invoiceNumber;
+	}
+	public void setInvoiceNumber(String invoiceNumber) {
+		this.invoiceNumber = invoiceNumber;
+	}
+	public String getTaxIDNumber() {
+		return taxIDNumber;
+	}
+	public void setTaxIDNumber(String taxIDNumber) {
+		this.taxIDNumber = taxIDNumber;
+	}
+	public String getRestaurantName() {
+		return restaurantName;
+	}
+	public void setRestaurantName(String restaurantName) {
+		this.restaurantName = restaurantName;
+	}
+	public String getCouponName() {
+		return couponName;
+	}
+	public void setCouponName(String couponName) {
+		this.couponName = couponName;
+	}
+	public Integer getProdNo() {
+		return prodNo;
+	}
+	public void setProdNo(Integer prodNo) {
+		this.prodNo = prodNo;
+	}
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	public Integer getProdQty() {
+		return prodQty;
+	}
+	public void setProdQty(Integer prodQty) {
+		this.prodQty = prodQty;
+	}
+	public Integer getProdPrice() {
+		return prodPrice;
+	}
+	public void setProdPrice(Integer prodPrice) {
+		this.prodPrice = prodPrice;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
 	@Override
 	public String toString() {
 		return "OrderSearchVO [prodOrderNo=" + prodOrderNo + ", memberNo=" + memberNo + ", restaurantNo=" + restaurantNo
@@ -73,249 +232,12 @@ public class OrderSearchVO implements java.io.Serializable{
 				+ amountAfterCoupon + ", prodOrderPoint=" + prodOrderPoint + ", prodOrderReceiverName="
 				+ prodOrderReceiverName + ", prodOrderReceiverTel=" + prodOrderReceiverTel + ", prodOrderReceiverMail="
 				+ prodOrderReceiverMail + ", prodOrderReceiverAddress=" + prodOrderReceiverAddress + ", invoiceNumber="
-				+ invoiceNumber + ", taxIDNumber=" + taxIDNumber + ", prodNo=" + prodNo + ", prodName=" + prodName
-				+ ", prodQty=" + prodQty + ", prodPrice=" + prodPrice + ", name=" + name + ", tel=" + tel + "]";
+				+ invoiceNumber + ", taxIDNumber=" + taxIDNumber + ", restaurantName=" + restaurantName
+				+ ", couponName=" + couponName + ", prodNo=" + prodNo + ", prodName=" + prodName + ", prodQty="
+				+ prodQty + ", prodPrice=" + prodPrice + ", name=" + name + ", tel=" + tel + "]";
 	}
-
 	
-	public Integer getProdOrderNo() {
-		return prodOrderNo;
-	}
-
-
-	public void setProdOrderNo(Integer prodOrderNo) {
-		this.prodOrderNo = prodOrderNo;
-	}
-
-
-	public Integer getMemberNo() {
-		return memberNo;
-	}
-
-
-	public void setMemberNo(Integer memberNo) {
-		this.memberNo = memberNo;
-	}
-
-
-	public Integer getRestaurantNo() {
-		return restaurantNo;
-	}
-
-
-	public void setRestaurantNo(Integer restaurantNo) {
-		this.restaurantNo = restaurantNo;
-	}
-
-
-	public Integer getCouponNo() {
-		return couponNo;
-	}
-
-
-	public void setCouponNo(Integer couponNo) {
-		this.couponNo = couponNo;
-	}
-
-
-	public String getOrderStatus() {
-		return orderStatus;
-	}
-
-
-	public void setOrderStatus(String orderStatus) {
-		this.orderStatus = orderStatus;
-	}
-
-
-	public java.sql.Timestamp getProdOrderDate() {
-		return prodOrderDate;
-	}
-
-
-	public void setProdOrderDate(java.sql.Timestamp prodOrderDate) {
-		this.prodOrderDate = prodOrderDate;
-	}
-
-
-	public java.sql.Timestamp getProdOrderReveiveTime() {
-		return prodOrderReveiveTime;
-	}
-
-
-	public void setProdOrderReveiveTime(java.sql.Timestamp prodOrderReveiveTime) {
-		this.prodOrderReveiveTime = prodOrderReveiveTime;
-	}
-
-
-	public java.sql.Timestamp getProdOderDeliverTime() {
-		return prodOderDeliverTime;
-	}
-
-
-	public void setProdOderDeliverTime(java.sql.Timestamp prodOderDeliverTime) {
-		this.prodOderDeliverTime = prodOderDeliverTime;
-	}
-
-
-	public Integer getDeliverFee() {
-		return deliverFee;
-	}
-
-
-	public void setDeliverFee(Integer deliverFee) {
-		this.deliverFee = deliverFee;
-	}
-
-
-	public Integer getAmountBeforeCoupon() {
-		return amountBeforeCoupon;
-	}
-
-
-	public void setAmountBeforeCoupon(Integer amountBeforeCoupon) {
-		this.amountBeforeCoupon = amountBeforeCoupon;
-	}
-
-
-	public Integer getAmountAfterCoupon() {
-		return amountAfterCoupon;
-	}
-
-
-	public void setAmountAfterCoupon(Integer amountAfterCoupon) {
-		this.amountAfterCoupon = amountAfterCoupon;
-	}
-
-
-	public Integer getProdOrderPoint() {
-		return prodOrderPoint;
-	}
-
-
-	public void setProdOrderPoint(Integer prodOrderPoint) {
-		this.prodOrderPoint = prodOrderPoint;
-	}
-
-
-	public String getProdOrderReceiverName() {
-		return prodOrderReceiverName;
-	}
-
-
-	public void setProdOrderReceiverName(String prodOrderReceiverName) {
-		this.prodOrderReceiverName = prodOrderReceiverName;
-	}
-
-
-	public String getProdOrderReceiverTel() {
-		return prodOrderReceiverTel;
-	}
-
-
-	public void setProdOrderReceiverTel(String prodOrderReceiverTel) {
-		this.prodOrderReceiverTel = prodOrderReceiverTel;
-	}
-
-
-	public String getProdOrderReceiverMail() {
-		return prodOrderReceiverMail;
-	}
-
-
-	public void setProdOrderReceiverMail(String prodOrderReceiverMail) {
-		this.prodOrderReceiverMail = prodOrderReceiverMail;
-	}
-
-
-	public String getProdOrderReceiverAddress() {
-		return prodOrderReceiverAddress;
-	}
-
-
-	public void setProdOrderReceiverAddress(String prodOrderReceiverAddress) {
-		this.prodOrderReceiverAddress = prodOrderReceiverAddress;
-	}
-
-
-	public String getInvoiceNumber() {
-		return invoiceNumber;
-	}
-
-
-	public void setInvoiceNumber(String invoiceNumber) {
-		this.invoiceNumber = invoiceNumber;
-	}
-
-
-	public String getTaxIDNumber() {
-		return taxIDNumber;
-	}
-
-
-	public void setTaxIDNumber(String taxIDNumber) {
-		this.taxIDNumber = taxIDNumber;
-	}
-
-
-	public Integer getProdNo() {
-		return prodNo;
-	}
-
-
-	public void setProdNo(Integer prodNo) {
-		this.prodNo = prodNo;
-	}
-
-
-	public String getProdName() {
-		return prodName;
-	}
-
-
-	public void setProdName(String prodName) {
-		this.prodName = prodName;
-	}
-
-
-	public Integer getProdQty() {
-		return prodQty;
-	}
-
-
-	public void setProdQty(Integer prodQty) {
-		this.prodQty = prodQty;
-	}
-
-
-	public Integer getProdPrice() {
-		return prodPrice;
-	}
-
-
-	public void setProdPrice(Integer prodPrice) {
-		this.prodPrice = prodPrice;
-	}
-
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public String getTel() {
-		return tel;
-	}
-
-
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
+	
 
 	
 }
