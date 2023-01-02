16adaec17157edd439dc6429e031f573a2270e14
Cindy <cindy5832@gmail.com>
2023-01-02 15:40:31 +0800

    新增星等js功能及修改訂位暫存功能

--- src/main/resources/static/Front_End/js/logged_restaurant.js
+++ src/main/resources/static/Front_End/js/logged_restaurant.js
@@ -6,6 +6,6 @@ if (authenticatedMember != null) {
     .parent()
     .empty()
     .append(
-      `<div class="testimonial__author__pic" style=" position : relative; top:-15px;right:-20px"><img class="member_pic" src="img/restaurant-login.png" alt="" style="object-fit: cover" /></div><a class="login_text" href="#" style="margin-top: 11px; display: inline-block">歡迎!${authenticatedMember.restaurantName}</a>`
+      `<div class="testimonial__author__pic" style=" position : relative; top:-15px;right:-20px"><img class="member_pic" src="img/restaurant-login.jpg" alt="" style="object-fit: cover" /></div><a class="login_text" href="#" style="margin-top: 11px; display: inline-block">歡迎!${authenticatedMember.restaurantName}</a>`
     );
 }
